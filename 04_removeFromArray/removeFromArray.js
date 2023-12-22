f7598254a2c1a194856c774ab1a0d3107140cd08
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-22 18:36:55 +0200

    Solved exercise 04 Exercise 04_removeFromArray solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...removables) {
+  return array.filter((num) => !removables.includes(num));
 };
 
 // Do not edit below this line
