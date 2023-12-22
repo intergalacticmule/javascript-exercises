4ee01a91fc0977c54e5c99fca7798e494d5dcba9
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-22 17:35:07 +0200

    Solved exercise 03 Exercise9 03_reverseString solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringReversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringReversed += string[i];
+  }
+  return stringReversed;
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
