246045fe611ffb02ae4ec9c37a7880622de740a3
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-22 17:22:39 +0200

    Solved exercise 02 Exercise 02_repeatString solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let myString = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      myString += string;
+    }
+    return myString;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
