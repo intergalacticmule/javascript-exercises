69a376f3785a22f347deb9fc28bb5e7e0239d420
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2023-12-22 19:16:25 +0200

    Solve exercise 05 Exercise 05_sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
 
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
+
+  let myArgs = [start, end];
+
+  if (start > end) {
+    myArgs = [end, start];
+  }
+
+  let sum = 0;
+
+  for (let i = myArgs[0]; i <= myArgs[1]; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
