cbc95230538a43baac18d9e8ed1cc05b5f796bda
Mario Georgiev Georgiev <MGGeorgiev@yettel.bg>
2024-01-17 15:59:58 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        let oldest_age = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let current_age = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldest_age > current_age ? oldest : current;
+    });
+};
 
+const getAge = function (yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        yearOfDeath = new Date().getFullYear();
+    }
+
+    return yearOfDeath - yearOfBirth;
 };
 
 // Do not edit below this line
