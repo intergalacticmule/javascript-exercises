af8e246cc7314c54a7b04bbb004a7f830e583409
Mario Georgiev Georgiev <MGGeorgiev@yettel.bg>
2024-01-17 13:23:50 +0200

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function () {};
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (typeof n === "string") {
+        n = parseInt(n);
+    }
+    let previousNumOne = 0;
+    let previousNumTwo = 1;
+
+    for (i = 0; i < n; i++) {
+        let currentNum = previousNumOne + previousNumTwo;
+        previousNumTwo = previousNumOne;
+        previousNumOne = currentNum;
+    }
+    return previousNumOne;
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
