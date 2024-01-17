a7a489b691a0ae1c5f063d83e20b196747b16e90
Mario Georgiev Georgiev <MGGeorgiev@yettel.bg>
2024-01-17 12:53:29 +0200

    Solve exercises 08, 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    processedWord = word
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    return processedWord.split("").reverse().join("") === processedWord;
 };
 
 // Do not edit below this line
